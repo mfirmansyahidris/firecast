3094cd0525bf0ab7d3c30efb2e339d9e80f09705
manca_fi <manca.fi@gmail.com>
2018-12-19 18:10:26 +0800

    crash analithycs and cloud messaging

---
+++ app/src/main/java/com/dev/fi/firecast/CloudMessagingService.kt
@@ -0,0 +1,47 @@
+package com.dev.fi.firecast
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+/**
+ ****************************************
+created by -manca-
+.::manca.fi@gmail.com ::.
+ ****************************************
+ */
+
+class CloudMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage?) {
+        sendMyNotification(message!!.notification!!.body)
+    }
+
+
+    private fun sendMyNotification(message: String?) {
+
+        //On click of notification it redirect to this Activity
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("My Firebase Push notification")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
--- app/src/main/java/com/dev/fi/firecast/MainActivity.kt
+++ app/src/main/java/com/dev/fi/firecast/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.dev.fi.firecast
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.ViewGroup
+import android.widget.Button
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            Crashlytics.getInstance().crash() // Force a crash
+        }
+
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true;
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
